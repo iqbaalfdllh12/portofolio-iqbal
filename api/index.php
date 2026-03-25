0688744f3b691907cb3b84b86e9767f0adaa3f99
Iqbal <iqbaalf12@gmail.com>
2026-03-25 13:03:37 +0700

    Fix: Force storage to tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,13 +1,11 @@
 <?php
 
-// Tampilkan error biar gak tebak-tebakan
+// Paksa folder temporary buat Laravel di Vercel
+putenv('APP_STORAGE=/tmp');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
+// Tampilkan error (biar gak putih doang kalau ada masalah)
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
-// Trik buat folder storage biar gak White Screen
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
-if (!is_dir('/tmp/storage/framework/views')) {
-    mkdir('/tmp/storage/framework/views', 0755, true);
-}
 
 require __DIR__ . '/../public/index.php';
