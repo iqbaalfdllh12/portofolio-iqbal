d102fb9f34e806f60b569e843425f2ea17d28770
Iqbal <iqbaalf12@gmail.com>
2026-03-25 12:56:10 +0700

    Fix: Hard reset storage and routes

--- api/index.php
+++ api/index.php
@@ -1,8 +1,13 @@
 <?php
 
-// Paksa nampilin error biar gak layar putih doang
+// Tampilkan error biar gak tebak-tebakan
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+// Trik buat folder storage biar gak White Screen
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
 
 require __DIR__ . '/../public/index.php';
